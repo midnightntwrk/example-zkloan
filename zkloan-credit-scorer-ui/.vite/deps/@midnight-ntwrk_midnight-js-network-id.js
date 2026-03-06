5e47913f8e24548257226b1a04428d7e17f5c2a4
JAlbertCode <JonathanAlbert0115@gmail.com>
2026-03-06 13:34:23 -0500

    Added .vite folders to git ignore and removed the files from the repo. Also removed node_modules from being ccommitted
    Added .vite folders to git ignore and removed the files from the repo. Also removed node_modules from being ccommitted

--- zkloan-credit-scorer-ui/.vite/deps/@midnight-ntwrk_midnight-js-network-id.js
+++
@@ -1,9 +0,0 @@
-import {
-  getNetworkId,
-  setNetworkId
-} from "./chunk-QU7K2XTK.js";
-import "./chunk-V4OQ3NZ2.js";
-export {
-  getNetworkId,
-  setNetworkId
-};
--- zkloan-credit-scorer-ui/.vite/deps/buffer.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_buffer
-} from "./chunk-D7WYEG3T.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_buffer();
