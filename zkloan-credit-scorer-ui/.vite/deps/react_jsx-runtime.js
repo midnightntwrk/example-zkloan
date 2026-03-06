515ac52964ed337bc1586d7588dd9961f257b13c
JAlbertCode <JonathanAlbert0115@gmail.com>
2026-03-06 13:34:23 -0500

    Added .vite folders to git ignore and removed the files from the repo. Also removed node_modules from being ccommitted
    Added .vite folders to git ignore and removed the files from the repo. Also removed node_modules from being ccommitted

--- zkloan-credit-scorer-ui/.vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-NSGMPQUM.js";
-import "./chunk-3LCTNZT5.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_jsx_runtime();
--- zkloan-credit-scorer-ui/.vite/deps/util.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_util
-} from "./chunk-ICY6TE7S.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_util();
